6c1681db4748a6b6b4985c62c3129e3f0d06bec2
DavidLRowe <david_rowe@sil.org>
2015-06-11 15:29:01 -0700

    provide optional XML formatting parameters for convCDtoXml.py

--- scripts/comp/convCDtoXml.py
+++ scripts/comp/convCDtoXml.py
@@ -3,18 +3,20 @@ __url__ = 'http://github.com/silnrsi/pysilfont'
 __copyright__ = 'Copyright (c) 2015, SIL International  (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
 __author__ = 'David Rowe'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from silfont.genlib import execute
 from silfont.genlib import ETWriter
 from silfont.composites import CompGlyph
 from xml.etree import ElementTree as ET
 
-# specify three parameters: input file (single line format), output file (XML format), log file.
+# specify three parameters: input file (single line format), output file (XML format), log file
+# and optional -p indentFirst "   " -p indentIncr "   " -p "PSName,UID,with,at,x,y" for XML formatting.
 argspec = [
     ('input',{'help': 'Input file of CD in single line format'}, {'type': 'infile'}),
     ('output',{'help': 'Output file of CD in XML format'}, {'type': 'outfile', 'def': '_out.xml'}),
-    ('log',{'help': 'Log file'},{'type': 'outfile', 'def': '_log.txt'})]
+    ('log',{'help': 'Log file'},{'type': 'outfile', 'def': '_log.txt'}),
+    ('-p','--params',{'help': 'XML formatting parameters: indentFirst, indentIncr, attOrder','action': 'append'}, {'type': 'optiondict'})]
 
 def doit(args) :
     ofile = args.output
@@ -44,7 +46,16 @@ def doit(args) :
         lfile.write("Valid glyphs found: " + str(elementcount)+'\n')
 #   instead of simple serialization with: ofile.write(ET.tostring(f))
 #   create ETWriter object and specify indentation and attribute order to get normalized output
-    etwobj=ETWriter(f, indentFirst='   ', indentIncr='   ', attributeOrder={'PSName':0,'UID':1,'with':2,'at':3,'x':4,'y':5})
+    indentFirst = "   "
+    indentIncr = "   "
+    attOrder = "PSName,UID,with,at,x,y"
+    for k in args.params:
+        if k == 'indentIncr': indentIncr = args.params['indentIncr']
+        elif k == 'indentFirst': indentFirst = args.params['indentFirst']
+        elif k == 'attOrder': attOrder = args.params['attOrder']
+    x = attOrder.split(',')
+    attributeOrder = dict(zip(x,range(len(x))))
+    etwobj=ETWriter(f, indentFirst=indentFirst, indentIncr=indentIncr, attributeOrder=attributeOrder)
     etwobj.serialize_xml(ofile.write)
     
     return
