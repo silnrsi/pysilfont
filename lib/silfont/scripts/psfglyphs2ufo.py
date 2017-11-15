832365cb571d61bcb640678d9cb47c089445ecb5
David Raymond <david_raymond@sil.org>
2017-11-15 10:52:17 +0000

    Updates to psfglyphs2ufo plus core and ufo libraries - psfgylphs2ufo now has many data fixes to enable round-tripping - core.py has support for ufo metadata checking parameters - data fixes now in psfgylphs2ufo now removed from ufo.py     and initial metatdata chcking code added - getval() in ufo.py now returns the correct data type rather     than just strings for integer and real values

--- lib/silfont/scripts/psfglyphs2ufo.py
+++ lib/silfont/scripts/psfglyphs2ufo.py
@@ -8,6 +8,8 @@ __author__ = 'Victor Gaultney'
 from silfont.core import execute
 
 import glyphsLib
+import silfont.ufo, os # Needed currently to read backup ufos from disk
+
 
 argspec = [
     ('glyphsfont', {'help': 'Input font file'}, {'type': 'filename'}),
@@ -21,27 +23,127 @@ def doit(args):
     logger.log("Creating UFO objects from GlyphsApp file", "I")
     ufos = glyphsLib.load_to_ufos(args.glyphsfont, propagate_anchors=False)
 
+    # Extract directory name for use with backups
+    (glyphsdir, filen) = os.path.split(args.glyphsfont)
+
+    librestorekeys = ("org.sil.pysilfontparams", "org.sil.altLineMetrics", "org.sil.lcg.toneLetters",
+                   "org.sil.lcg.transforms", "public.glyphOrder", "public.postscriptNames")
+    libdeletekeys = ("UFOFormat", "com.schriftgestaltung.blueFuzz", "com.schriftgestaltung.blueScale",
+                     "com.schriftgestaltung.blueShift")
+    libdeleteempty = ("com.schriftgestaltung.DisplayStrings")
+
+    inforestorekeys = ("openTypeHeadCreated", "openTypeNamePreferredFamilyName", "openTypeNamePreferredSubfamilyName",
+                       "openTypeNameUniqueID", "openTypeOS2WeightClass", "openTypeOS2WidthClass", "postscriptFontName",
+                       "postscriptFullName", "styleMapFamilyName", "styleMapStyleName", "styleName")
+    integerkeys = ("openTypeOS2WeightClass", "openTypeOS2WidthClass")
+    infodeleteempty = ("openTypeOS2Selection", "postscriptFamilyBlues", "postscriptFamilyOtherBlues",
+                       "postscriptOtherBlues")
+    infodelete = ("guidelelines", "openTypeOS2Type")
+
     for ufo in ufos:
+        fontname = ufo.info.familyName.replace(" ", "") + "-" + ufo.info.styleName
         # Fixes to the data
         if not args.nofixes:
-            # glyphOrder has data as unicode rather than utf-8.
-            new = []
-            # changes = False
-            for i,name in enumerate(ufo.lib["public.glyphOrder"]):
-                if name == float("inf"):
-                    print i
-                    name = "Infinity"
-                    logger.log("Infinity value corrected in public.glyphOrder", "I")
-                #if isinstance(name, unicode):
-                #    name = name.encode('utf8')
-                #    changes = True
-                new.append(name)
-            # if changes: logger.log("Unicode fixes made to public.glyphOrder")
-            ufo.lib["public.glyphOrder"] = new
+            logger.log("Fixing data in " + fontname, "P")
+            # lib.plist processing
+            logger.log("Checking lib.plist", "P")
+
+            libplist = ufo.lib
+
+            # Process UFO.lib if present
+            if "UFO.lib" in libplist:
+                logger.log("UFOlib found in lib.plist for " + fontname + ". Values will be copied to root", "P")
+                ul = libplist["UFO.lib"]
+                # Copy fields from UFO.lib to root
+                for key in ul:
+                    if key in librestorekeys:
+                        continue # They will be restored later
+                    if key in libdeleteempty:
+                        if ul[key] == "" :
+                            logger.log("Emtpy field ignored: " + key, "I")
+                            continue
+                    if key in libdeletekeys:
+                        logger.log(key + " ignored", "I")
+                        continue
+                    if key in ufo.lib:
+                        current = ufo.lib[key]
+                        logmess = " updated from UFO.lib. "
+                    else:
+                        current = None
+                        logmess = " copied from UFO.lib. "
+                    new = ul[key]
+                    if current == new:
+                        continue
+                    else:
+                        ufo.lib[key] = new
+                        logchange(logger, logmess, key, current, new)
+                del ufo.lib["UFO.lib"]
+                logger.log("UFO.lib field deleted", "I")
+
+            # Restore values from backups - currently from fontname-bk.ufo in same folder as input .gylphs file
+            bklibplist = silfont.ufo.Uplist(font=None, dirn=os.path.join(glyphsdir,fontname+"-bk.ufo"),
+                                            filen="lib.plist")
+
+            for key in librestorekeys:
+                if key in bklibplist:
+                    new = bklibplist.getval(key)
+                    current = None if key not in ufo.lib else ufo.lib[key]
+                    if current == new:
+                        continue
+                    else:
+                        ufo.lib[key] = new
+                        logchange(logger, " restored from backup ufo. ", key, current, new)
+
+            for key in libdeletekeys:
+                if key in ufo.lib:
+                    current = ufo.lib[key]
+                    del ufo.lib[key]
+                    logchange(logger, " deleted. ", key, current, None)
+
+            # fontinfo.plist processing
+
+            logger.log("Checking fontinfo.plist", "P")
+
+            fontinfo = ufo.info
+            bkfontinfo = silfont.ufo.Uplist(font=None, dirn=os.path.join(glyphsdir, fontname + "-bk.ufo"),
+                                            filen="fontinfo.plist")
+            for key in inforestorekeys:
+                if key in bkfontinfo:
+                    new = bkfontinfo.getval(key)
+                    if key in integerkeys: new = int(new)
+                    current = None if not hasattr(ufo.info, key) else  getattr(ufo.info, key)
+                    if current == new:
+                        continue
+                    else:
+                        setattr(ufo.info, key, new)
+                        logchange(logger, " restored from backup ufo. ", key, current, new)
 
         # Write ufo out
-        logger.log("Writing out " + ufo.info.familyName + " " + ufo.info.styleName, "P")
+        logger.log("Writing out " + fontname, "P")
         glyphsLib.write_ufo(ufo, args.masterdir)
+
+def logchange(logger, logmess, key, old, new):
+    oldstr = str(old) if len(str(old)) < 22 else str(old)[0:20] + "..."
+    newstr = str(new) if len(str(new)) < 22 else str(new)[0:20] + "..."
+    logmess = key + logmess
+    if old is None:
+        logmess = logmess + " New value: " + newstr
+    else:
+        if new is None:
+            logmess = logmess + " Old value: " + oldstr
+        else:
+            logmess = logmess + " Old value: " + oldstr + ", new value: " + newstr
+    logger.log(logmess, "I")
+    # Extra verbose logging
+    if len(str(old)) > 21 :
+        logger.log("Full old value: " + str(old), "V")
+    if len(str(new)) > 21 :
+        logger.log("Full new value: " + str(new), "V")
+    logger.log("Types: Old - " + str(type(old)) + ", New - " + str(type(new)), "V")
+
+
+
+
 
 
 def cmd(): execute(None, doit, argspec)
