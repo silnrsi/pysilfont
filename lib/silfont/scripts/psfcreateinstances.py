ca3e0cb79f468dbff1926a7f69f23584fa00456d
Alan Ward <nrsiward@users.noreply.github.com>
2018-04-11 14:04:03 -0500

    Supported building one instance UFO. Provided a log file by default.

--- lib/silfont/scripts/psfcreateinstances.py
+++ lib/silfont/scripts/psfcreateinstances.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
-'Generate instance UFO(s) from a designspace document and master UFO(s)'
+'Generate instance UFOs from a designspace document and master UFOs'
+
+# Python 2.7 script to build instance UFOs from a designspace document
+# If a file is given, all instances are built
+# If a folder is given, all instances in all designspace files are built
+# A particular instance to build can be specified
+#  though only if a file (not a folder) is specified
+# The particular instance can be identified using the -i option
+#  and the 'name' attribute value for an 'instance' element in the designspace file
+# Or it can be identified using the -a and -v options
+#  to specify any attribute and value pair for an 'instance' in the designspace file
+# If more than one instances matches, both will be built
+
 __url__ = 'http://github.com/silnrsi/pysilfont'
 __copyright__ = 'Copyright (c) 2018 SIL International  (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
 __author__ = 'Alan Ward'
 
-#from mutatorMath.ufo.document import DesignSpaceDocumentReader
+import os
+from mutatorMath.ufo.document import DesignSpaceDocumentReader
 from mutatorMath.ufo import build as build_designspace
 from silfont.core import execute
 
 argspec = [
-    ('designspace_path', {'help': 'Path to designspace document or folder of them'}, {}),
+    ('designspace_path', {'help': 'Path to designspace document or a folder of them'}, {}),
+    ('-i', '--instanceName', {'help': 'Font name for instance to build', 'action': 'store'}, {}),
+    ('-a', '--instanceAttr', {'help': 'Attribute used to specify instance to build'}, {}),
+    ('-v', '--instanceVal', {'help': 'Value of attribute specifying instance to build'}, {}),
     ('--roundInstances', {'help': 'Apply integer rounding to all geometry when interpolating',
                            'action': 'store_true'}, {}),
-    ('--progress', {'help': 'Show progress and errors', 'action': 'store_true'}, {})
+    ('--progress', {'help': 'Show progress and errors', 'action': 'store_true'}, {}),
+    ('-l','--log',{'help': 'Log file (default: *_createinstances.log)'}, {'type': 'outfile', 'def': '_createinstances.log'}),
 ]
 
 logger = None
@@ -30,31 +47,60 @@ def doit(args):
     args.logger.log('Interpolating master UFOs from designspace', 'P')
 
     designspace_path = args.designspace_path
-    roundInstances = args.roundInstances
-    showProgress = args.progress
+    instance_font_name = args.instanceName
+    instance_attr = args.instanceAttr
+    instance_val = args.instanceVal
+    round_instances = args.roundInstances
+    show_progress = args.progress
 
-    # The below is an alternative implementation
-    #  based loosely on fontmake's FontProject.run_from_designspace()
-    #   but it always interpolates all instances
-    #   and only generates those instances without building ttfs
-    #
-    # reader = DesignSpaceDocumentReader(designspace_path, ufoVersion=3,
-    #                                    roundGeometry=round_instances)
-    # reader.readInstances()
+    if instance_font_name and (instance_attr or instance_val):
+        args.logger.log('--instanceName is mutually exclusive with --instanceAttr or --instanceVal','S')
+    if (instance_attr and not instance_val) or (instance_val and not instance_attr):
+        args.logger.log('--instanceAttr and --instanceVal must be used together', 'S')
 
-    # The below simply uses a utility function that's part of mutatorMath
-    #  In addition to accepting a designspace file,
-    #   it will accept a folder and processes all designspace files there
-    #  It can also produce progress and error messages
-    progressFn = progressFunc if showProgress else None
-    build_designspace(designspace_path,
-                      outputUFOFormatVersion=3, roundGeometry=roundInstances,
-                      progressFunc=progressFn)
+    progress_func = progressFunc if show_progress else None
+
+    if instance_font_name or instance_attr:
+        if not os.path.isfile(designspace_path):
+            args.logger.log('Specifying an instance requires a designspace file--not a folder', 'S')
+        reader = DesignSpaceDocumentReader(designspace_path, ufoVersion=3,
+                                           roundGeometry=round_instances,
+                                           progressFunc=progress_func)
+        key_attr = instance_attr if instance_val else 'name'
+        key_val = instance_val if instance_attr else instance_font_name
+        reader.readInstance((key_attr, key_val))
+    else:
+        # The below uses a utility function that's part of mutatorMath
+        #  In addition to accepting a designspace file,
+        #  it will accept a folder and processes all designspace files there
+        progressFn = progressFunc if show_progress else None
+        build_designspace(designspace_path,
+                          outputUFOFormatVersion=3, roundGeometry=round_instances,
+                          progressFunc=progress_func)
+
+        # The below is an alternative implementation that uses DesignSpaceDocumentReader
+        #  instead of the build_designspace utililty function
+        # It's based loosely on fontmake's FontProject.run_from_designspace()
+        #  but it always interpolates all instances
+        #  and only generates those instances without building ttfs
+        # It does NOT accept a folder of designspace files
+        reader = DesignSpaceDocumentReader(designspace_path, ufoVersion=3,
+                                           roundGeometry=round_instances,
+                                           progressFunc=progress_func)
+        reader.readInstances()
+
+    args.logger.log('Done', 'P')
 
 def cmd(): execute(None, doit, argspec)
 if __name__ == '__main__': cmd()
 
+# Future development might use: fonttools\Lib\fontTools\designspaceLib to read
+#  the designspace file (which is the most up-to-date approach)
+#  then pass that object to mutatorMath, but there's no way to do that today.
+#
+#
 # For reference:
+# from mutatorMath/ufo/__init__.py:
 # 	build() is a convenience function for reading and executing a designspace file.
 # 		documentPath: 				filepath to the .designspace document
 # 		outputUFOFormatVersion:		ufo format for output
@@ -63,6 +109,13 @@ if __name__ == '__main__': cmd()
 # 		progressFunc:				an optional callback to report progress.
 # 									see mutatorMath.ufo.tokenProgressFunc
 #
-# Future development might use: fonttools\Lib\fontTools\designspaceLib to read
-#  the designspace file (which is the most up-to-date approach)
-#  then pass that object to mutatorMath, but there's no way to do that today.
+# class DesignSpaceDocumentReader(object):
+# def __init__(self, documentPath,
+#         ufoVersion,
+#         roundGeometry=False,
+#         verbose=False,
+#         logPath=None,
+#         progressFunc=None
+#         ):
+#
+# def readInstance(self, key, makeGlyphs=True, makeKerning=True, makeInfo=True):
