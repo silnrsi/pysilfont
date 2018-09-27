bfc50672b5a62a51400ab058ab67a9f1c95512fc
David Raymond <david_raymond@sil.org>
2018-09-27 19:44:40 +0100

    Added unicode_literals to psfremovegliflibkeys

--- lib/silfont/scripts/psfremovegliflibkeys.py
+++ lib/silfont/scripts/psfremovegliflibkeys.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import unicode_literals
 '''Remove the specified key(s) from glif libs'''
 __url__ = 'http://github.com/silnrsi/pysilfont'
 __copyright__ = 'Copyright (c) 2017 SIL International (http://www.sil.org)'
