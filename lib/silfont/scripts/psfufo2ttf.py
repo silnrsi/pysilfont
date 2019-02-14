b629ee7cbf44bd7765df4efb466489b17ee4eff5
Martin Hosken <martin_hosken@sil.org>
2019-02-14 11:58:28 +0700

    Add UVS support to psfufo2ttf

--- lib/silfont/scripts/psfufo2ttf.py
+++ lib/silfont/scripts/psfufo2ttf.py
@@ -19,6 +19,19 @@ argspec = [
 
 PUBLIC_PREFIX = 'public.'
 
+def getuvss(ufo):
+    uvsdict = {}
+    for g in ufo:
+        uvs = getattr(g, 'lib', {}).get("org.sil.uvs", None)
+        if uvs is None:
+            continue
+        codes = [int(x, 16) for x in uvs.split()]
+        if codes[1] not in uvsdict:
+            uvsdict[codes[1]] = []
+        uvsdict[codes[1]].append((codes[0], (g.name if codes[0] not in g.unicodes else None)))
+    return uvsdict
+        
+
 def doit(args):
     ufo = defcon.Font(args.iufo)
 
@@ -36,6 +49,17 @@ def doit(args):
         glyphSet=glyphSet,
         glyphOrder=ufo.lib.get(PUBLIC_PREFIX + 'glyphOrder'))
     font = outlineCompiler.compile()
+
+    # handle uvs glyphs until ufo2ft does it for us.
+    uvsdict = getuvss(ufo)
+    if len(uvsdict):
+        from fontTools.ttLib.tables._c_m_a_p import cmap_format_14
+        cmap_uvs = cmap_format_14(14)
+        cmap_uvs.platformID = 0
+        cmap_uvs.platEncID = 5
+        cmap_uvs.cmap = {}
+        cmap_uvs.uvsDict = uvsdict
+        font['cmap'].tables.append(cmap_uvs)
 
     args.logger.log('Saving ttf file', 'P')
     font.save(args.ottf)
