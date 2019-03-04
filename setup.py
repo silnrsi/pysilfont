f04f4a809378ff62cc90757fb88e3745b9d5875e
David Raymond <david_raymond@sil.org>
2019-03-04 15:21:22 +0000

    Updated pysilfont version to 1.4.2.dev0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 __url__ = 'http://github.com/silnrsi/pysilfont'
 __copyright__ = 'Copyright (c) 2014 SIL International (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
-__version__ = '1.4.1'
+__version__ = '1.4.2.dev0'
 
 import sys, os, imp
 
