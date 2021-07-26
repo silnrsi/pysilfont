6ff9c7b0ed2f1790dc0063cc739245c5d660ab6d
Bob Hallissy <bobh0303@users.noreply.github.com>
2021-07-26 16:30:15 -0600

    Update copyright

--- examples/psfgenftml.py
+++ examples/psfgenftml.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 'generate ftml tests from glyph_data.csv and UFO'
 __url__ = 'http://github.com/silnrsi/pysilfont'
-__copyright__ = 'Copyright (c) 2018 SIL International  (http://www.sil.org)'
+__copyright__ = 'Copyright (c) 2018,2021 SIL International  (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
 __author__ = 'Bob Hallissy'
 
