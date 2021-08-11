3ee87040b3ad75d870dfbdc3d32f10aaaa56d201
Bobby de Vos <bobby_devos@sil.org>
2021-08-11 09:55:40 -0600

    Handle multiple fonts

--- lib/silfont/scripts/psfshownames.py
+++ lib/silfont/scripts/psfshownames.py
@@ -5,8 +5,9 @@ __copyright__ = 'Copyright (c) 2021 SIL International (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
 __author__ = 'Bobby de Vos'
 
-from silfont.core import execute
+from silfont.core import execute, splitfn
 from fontTools.ttLib import TTFont
+import glob
 
 WINDOWS_ENGLISH_IDS = 3, 1, 0x409
 
@@ -22,8 +23,7 @@ FAMILY_RELATED_IDS = {
 }
 
 argspec = [
-    ('infont', {'help': 'TTF file'}, {}),
-    ('-l', '--log', {'help': 'Log file'}, {'type': 'outfile', 'def': '_namefields.log'}),
+    ('fonts', {'help': 'ttf font(s) to run report against; wildcards allowed', 'nargs': "+"}, {'type': 'filename'})
 ]
 
 
@@ -31,15 +31,39 @@ def doit(args):
     logger = args.logger
     width = max([len(name_id_name) for name_id_name in FAMILY_RELATED_IDS.values()]) + 1
 
-    font = TTFont(args.infont)
-    names(width, font)
+    fonts = []
+    maxwidth = 0
+    for pattern in args.fonts:
+        for fullpath in glob.glob(pattern):
+            (path, base, ext) = splitfn(fullpath)
+            if ext != ".ttf":
+                logger.log("Fonts must ttf - " + fullpath + " invalid", "S")
+            maxwidth = max(maxwidth, len(base))
+            fonts.append((fullpath, path, base, ext))
+    if fonts == []:
+        logger.log("No files match the filespec provided for fonts: " + str(args.fonts), "S")
+
+    for (fullpath, path, base, ext) in fonts:
+        logger.log(f'Processing {fullpath}', 'P')
+        try:
+            font = TTFont(fullpath)
+        except Exception as e:
+            logger.log(f'Error opening {fullpath}: {e}', 'E')
+            break
+
+        fontname = f'{base:{maxwidth}}'
+        records = names(width, font, fontname)
+        if args.quiet:
+            print(records[1:])
+        else:
+            logger.log("The following family-related values were found in the name table" + records, "P")
 
 
-def names(width, font):
-    """Print various name table fields and other related data."""
-
+def names(width, font, fontname):
     table = font['name']
     (platform_id, encoding_id, language_id) = WINDOWS_ENGLISH_IDS
+
+    records = ''
     for name_id in sorted(FAMILY_RELATED_IDS.keys()):
         name_id_name = FAMILY_RELATED_IDS[name_id] + ':'
         record = table.getName(
@@ -49,8 +73,13 @@ def names(width, font):
             langID=language_id
         )
         if record:
-            print(f'{name_id:2}: {name_id_name:{width}} {record}')
+            records += f'\n{fontname} {name_id:2}: {name_id_name:{width}} {record}'
+
+    return records
+
+
+def cmd(): execute('FT', doit, argspec)
 
 
 if __name__ == '__main__':
-    execute('FT', doit, argspec)
+    cmd()
