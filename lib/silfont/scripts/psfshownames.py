8b3f954a507fadcacdb2ca3660f2005f6e811ff9
Bobby de Vos <bobby_devos@sil.org>
2021-10-27 09:44:21 -0600

    Use a data structure to hold font information
    This will enable new functionality in the future.

--- lib/silfont/scripts/psfshownames.py
+++ lib/silfont/scripts/psfshownames.py
@@ -22,6 +22,21 @@ FAMILY_RELATED_IDS = {
     22: 'WWS subfamily',
 }
 
+
+class FontInfo:
+    def __init__(self):
+        self.filename = ''
+        self.name_table = dict()
+        self.weight_class = 0
+        self.regular = ''
+        self.bold = ''
+        self.italic = ''
+        self.width = ''
+        self.width_name = ''
+        self.width_class = 0
+        self.wws = ''
+
+
 argspec = [
     ('font', {'help': 'ttf font(s) to run report against; wildcards allowed', 'nargs': "+"}, {'type': 'filename'}),
     ('-b', '--bits', {'help': 'Show bits', 'action': 'store_true'}, {}),
@@ -33,6 +48,7 @@ def doit(args):
     name_width = max([len(name_id_name) for name_id_name in FAMILY_RELATED_IDS.values()]) + 1
 
     fonts = []
+    font_infos = []
     filename_width = 0
     for pattern in args.font:
         for fullpath in glob.glob(pattern):
@@ -50,26 +66,31 @@ def doit(args):
             logger.log(f'Error opening {fullpath}: {e}', 'E')
             break
 
+        font_info = FontInfo()
+        font_info.filename = fullpath
+        names(font, font_info)
+        bits(font, font_info)
+        font_infos.append(font_info)
+
+    for font_info in font_infos:
         filename = ''
-        if len(fonts) > 1:
-            filename = fullpath + ':'
+        if len(font_infos) > 1:
+            filename = font_info.filename + ':'
             filename = f'{filename:{filename_width}} '
-        records = names(name_width, font, filename)
+        records = multiline_names(name_width, font_info, filename)
         if args.bits:
-            records += bits(name_width, font, filename)
+            records += multiline_bits(name_width, font_info, filename)
         if args.quiet:
             print(records[1:])
         else:
             logger.log("The following family-related values were found in the name, head, and OS/2 tables" + records, "P")
 
 
-def names(name_width, font, filename):
+def names(font, font_info):
     table = font['name']
     (platform_id, encoding_id, language_id) = WINDOWS_ENGLISH_IDS
 
-    records = ''
-    for name_id in sorted(FAMILY_RELATED_IDS.keys()):
-        name_id_name = FAMILY_RELATED_IDS[name_id] + ':'
+    for name_id in FAMILY_RELATED_IDS:
         record = table.getName(
             nameID=name_id,
             platformID=platform_id,
@@ -77,64 +98,49 @@ def names(name_width, font, filename):
             langID=language_id
         )
         if record:
-            records += f'\n{filename}{name_id:2}: {name_id_name:{name_width}} {record}'
+            font_info.name_table[name_id] = str(record)
 
+
+def bits(font, font_info):
+    os2 = font['OS/2']
+    head = font['head']
+    font_info.weight_class = os2.usWeightClass
+    font_info.regular = bit2code(os2.fsSelection, 6, 'W-')
+    font_info.bold = bit2code(os2.fsSelection, 5, 'W')
+    font_info.bold += bit2code(head.macStyle, 0, 'M')
+    font_info.italic = bit2code(os2.fsSelection, 0, 'W')
+    font_info.italic += bit2code(head.macStyle, 1, 'M')
+    font_info.width_class = os2.usWidthClass
+    font_info.width = str(font_info.width_class)
+    if font_info.width_class == 5:
+        font_info.width_name = 'Width-Normal'
+    if font_info.width_class < 5:
+        font_info.width_name = 'Width-Condensed'
+        font_info.width += bit2code(head.macStyle, 5, 'M')
+    if font_info.width_class > 5:
+        font_info.width_name = 'Width-Extended'
+        font_info.width += bit2code(head.macStyle, 6, 'M')
+    font_info.wws = bit2code(os2.fsSelection, 8, '8')
+
+
+def multiline_names(name_width, font_info, filename):
+    records = ''
+    for name_id in sorted(font_info.name_table):
+        name_id_name = FAMILY_RELATED_IDS[name_id] + ':'
+        record = font_info.name_table[name_id]
+        records += f'\n{filename}{name_id:2}: {name_id_name:{name_width}} {record}'
     return records
 
 
-def bits(name_width, font, filename):
-    os2 = font['OS/2']
-    head = font['head']
-    records = list()
-
-    record = bit_record(filename, 'usWeightClass', name_width, os2.usWeightClass)
-    records.append(record)
-
-    codes = ''
-    codes += bit2code(os2.fsSelection, 6, 'W-')
-    if codes != '':
-        record = bit_record(filename, 'Regular', name_width, codes)
-        records.append(record)
-
-    codes = ''
-    codes += bit2code(os2.fsSelection, 5, 'W')
-    codes += bit2code(head.macStyle, 0, 'M')
-    if codes != '':
-        record = bit_record(filename, 'Bold', name_width, codes)
-        records.append(record)
-
-    codes = ''
-    codes += bit2code(os2.fsSelection, 0, 'W')
-    codes += bit2code(head.macStyle, 1, 'M')
-    if codes != '':
-        record = bit_record(filename, 'Italic', name_width, codes)
-        records.append(record)
-
-    us_width_class = os2.usWidthClass
-    codes = ''
-    codes += str(us_width_class)
-    if us_width_class == 5:
-        if codes != '':
-            record = bit_record(filename, 'Width-Normal', name_width, codes)
-            records.append(record)
-    if us_width_class < 5:
-        codes += bit2code(head.macStyle, 5, 'M')
-        if codes != '':
-            record = bit_record(filename, 'Width-Condensed', name_width, codes)
-            records.append(record)
-    if us_width_class > 5:
-        codes += bit2code(head.macStyle, 6, 'M')
-        if codes != '':
-            record = bit_record(filename, 'Width-Extended', name_width, codes)
-            records.append(record)
-
-    codes = ''
-    codes += bit2code(os2.fsSelection, 8, '8')
-    if codes != '':
-        record = bit_record(filename, 'WWS', name_width, codes)
-        records.append(record)
-
-    return '\n' + '\n'.join(records)
+def multiline_bits(name_width, font_info, filename):
+    records = ''
+    records += bit_record(filename, 'usWeightClass', name_width, font_info.weight_class)
+    records += bit_record(filename, 'Regular', name_width, font_info.regular)
+    records += bit_record(filename, 'Bold', name_width, font_info.bold)
+    records += bit_record(filename, 'Italic', name_width, font_info.italic)
+    records += bit_record(filename, font_info.width_name, name_width, font_info.width)
+    records += bit_record(filename, 'WWS', name_width, font_info.wws)
+    return records
 
 
 def bit2code(bit_field, bit, code_letter):
@@ -145,9 +151,11 @@ def bit2code(bit_field, bit, code_letter):
 
 
 def bit_record(filename, bit_field_name, name_width, codes):
-    bit_field_name += ':'
-    record = f'{filename}    {bit_field_name:{name_width}} {codes}'
-    return record
+    if codes:
+        bit_field_name += ':'
+        record = f'\n{filename}    {bit_field_name:{name_width}} {codes}'
+        return record
+    return ''
 
 
 def cmd(): execute('FT', doit, argspec)
