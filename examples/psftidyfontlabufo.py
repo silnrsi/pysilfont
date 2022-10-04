d69a21b855663c1cede7154a10caecb0224fb17e
David Raymond <david_raymond@sil.org>
2022-10-04 10:07:06 +0100

    Updates to example script used testing Fontlab workflow

--- examples/psftidyfontlabufo.py
+++ examples/psftidyfontlabufo.py
@@ -17,6 +17,7 @@ from silfont.core import execute, splitfn
 from xml.etree import ElementTree as ET
 from silfont.ufo import Ufont
 import os, glob
+from difflib import ndiff
 
 argspec = [
     ('ifont',{'help': 'post-fontlab ufo'}, {'type': 'infont'}),
@@ -37,6 +38,7 @@ def doit(args) :
         logger.log("No backups found matching %s so aborting..." % backuppath, "P")
         return
     backupname = sorted(backups)[0]  # Choose the oldest backup - date/time format sorts alphabetically
+    logger.log(f"Opening backup font {backupname}", "P")
     bfont = Ufont(backupname, params=params)
     outufoname = os.path.join(path, base + ".tidied.ufo")
 
@@ -48,10 +50,13 @@ def doit(args) :
             logger.log(f"layer {layer.layername} missing", "E")
             continue
         fllayer = fllayers[layer.layername]
+        glifchangecount = 0
         smoothchangecount = 0
+        duplicatenodecount = 0
         compchangecount = 0
         for gname in layer:
             glif = layer[gname]
+            glifchange = False
             flglif = fllayer[gname]
             if "outline" in glif and "outline" in flglif:
                 changestomake = []
@@ -64,26 +69,45 @@ def doit(args) :
                 for contour in contours:
                     flc = next(flcontours)
                     points = contour["point"]
-                    if len(points) != len(flc["point"]): # Contours must be different
-                        otherchange = True
+                    flpoints = flc["point"]
+                    duplicatenode = False
+                    smoothchanges = True
+                    if len(points) != len(flpoints): # Contours must be different!
+                        if len(flpoints) - len(points) == 1: # Look for duplicate node issue
+                            (different, plus, minus) = sdiff(str(ET.tostring(points[0]).strip()), str(ET.tostring(flpoints[0]).strip()))
+                            if ET.tostring(points[0]).strip() == ET.tostring(flpoints[-1]).strip(): # With duplicate node issue first point is appended to the end
+                                if plus == "lin" and minus == "curv": # On first point curve changed to line.
+                                    duplicatenode = True # Also still need check all the remaining points are the same
+                                    break                # but next check does that
+                        otherchange = True # Duplicate node issue above is only case where contour count can be different
                         break
-                    flpoints = iter(flc["point"])
+
+                    firstpoint = True
                     for point in points:
-                        flp = next(flpoints)
-                        xml = ET.tostring(point).strip()
-                        flxml = ET.tostring(flp).strip()
-                        if xml != flxml: # points are different
-                            if xml[:-2] + b'smooth="yes" />' == flxml or xml == flxml[:-2] + b'smooth="yes" />':
-                                changestomake = True # Only difference is addition or removal of smooth="yes"
+                        flp = flpoints.pop(0)
+                        if firstpoint and duplicatenode: # Ignore the first point since that will be different
+                            firstpoint = False
+                            continue
+                        firstpoint = False
+                        (different, plus, minus) = sdiff(str(ET.tostring(point).strip()), str(ET.tostring(flp).strip()))
+                        if different: # points are different
+                            if plus.strip() + minus.strip() == 'smooth="yes"':
+                                smoothchanges = True # Only difference is addition or removal of smooth="yes"
                             else: # Other change to glif,so can't safely make changes
                                 otherchange = True
 
-                if changestomake and not otherchange: # Only changes to contours in glif are addition(s) or removal(s) of smooth="yes"
+                if (smoothchanges or duplicatenode) and not otherchange: # Only changes to contours in glif are known issues that should be reset
                     flcontours = iter(floutline.contours)
                     for contour in list(contours):
                         flcontour = next(flcontours)
                         outline.replaceobject(contour, flcontour, "contour")
-                    smoothchangecount += 1
+                    if smoothchanges:
+                        logger.log(f'Smooth changes made to {gname}', "I")
+                        smoothchangecount += 1
+                    if duplicatenode:
+                        logger.log(f'Duplicate node changes made to {gname}', "I")
+                        duplicatenodecount += 1
+                    glifchange = True
 
                 # Now need to move components to the front...
                 components = outline.components
@@ -91,12 +115,26 @@ def doit(args) :
                     oldcontours = list(contours) # Easiest way to 'move' components is to delete contours then append back at the end
                     for contour in oldcontours: outline.removeobject(contour, "contour")
                     for contour in oldcontours: outline.appendobject(contour, "contour")
+                    logger.log(f'Component position changes made to {gname}', "I")
                     compchangecount += 1
+                    glifchange = True
+                if glifchange: glifchangecount += 1
 
-        logger.log(f'{layer.layername}: {smoothchangecount} glifs changed due to smooth and {compchangecount} glifs changed due to components position', "P")
+        logger.log(f'{layer.layername}: {glifchangecount} glifs changed', 'P')
+        logger.log(f'{layer.layername}: {smoothchangecount} changes due to smooth, {duplicatenodecount} due to duplicate nodes and {compchangecount} due to components position', "P")
 
     bfont.write(outufoname)
     return
+
+def sdiff(before, after): # Returns strings with the differences between the supplited strings
+    if before == after: return(False,"","") # First returned value is True if the strings are different
+    diff = ndiff(before, after)
+    plus = ""  # Plus will have the extra characters that are only in after
+    minus = "" # Minus will have the characters missing from after
+    for d in diff:
+        if d[0] == "+": plus += d[2]
+        if d[0] == "-": minus += d[2]
+    return(True, plus, minus)
 
 def cmd() : execute("UFO",doit, argspec)
 if __name__ == "__main__": cmd()
