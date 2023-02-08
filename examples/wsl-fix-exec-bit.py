56d8842788371dc9013b74cda69c46eb85ece35e
Nicolas Spalinger <nicolas.spalinger@sil.org>
2023-02-08 14:15:27 -0600

    This script will live elsewhere.

--- examples/wsl-fix-exec-bit.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-'''simple script to fix WSL's executable bit limitations'''
-
-import subprocess
-
-print("Fixing the execute bit in the current git local clone")
-
-subprocess.call('chmod -c u+x $(git ls-files --stage | grep -e "^100755" | cut -f2)', shell=True)
