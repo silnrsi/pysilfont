e50ac9e3b3cfef0903cc2df247428d49e0f1962d
David Raymond <david_raymond@sil.org>
2023-07-19 14:15:31 +0100

    Updated psfbuildcomp to use g_blue,g_purple as the default colours for -c

--- lib/silfont/scripts/psfbuildcomp.py
+++ lib/silfont/scripts/psfbuildcomp.py
@@ -44,7 +44,7 @@ def doit(args):
 
     colors = None
     if args.color or args.colors:
-        colors = args.colors if args.colors else "(0.04,0.57,0.04,1)"
+        colors = args.colors if args.colors else "g_blue,g_purple"
         colors = parsecolors(colors, allowspecial=True)
         invalid = False
         for color in colors:
