2052daca2d928f6331b131e46f4ffbc0faf9b2a2
Nicolas Spalinger <nicolas.spalinger@sil.org>
2023-08-02 16:15:46 -0500

    Updated python base tools for installation/build, updated editable path by default, dropped explicit branch names to rely on implicit default config instead

--- preflight/update-preflight-libs-pyenv.sh
+++ preflight/update-preflight-libs-pyenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Update preflight libs (assumes a pyenv approach)
 
-# Copyright (c) 2022, SIL International  (https://www.sil.org)
+# Copyright (c) 2023, SIL International  (https://www.sil.org)
 # Released under the MIT License (https://opensource.org/licenses/MIT)
 # maintained by Nicolas Spalinger
 
@@ -17,18 +17,20 @@ pyenv versions
 echo ""
 
 echo "Installing/Updating pip"
-python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip setuptools wheel setuptools_scm
 echo ""
 
 echo "Populating/updating the preflight dependencies for the active pyenv interpreter"  
 
+# components in editable mode:
+# (with source at the root of the user's home directory so that src/ folder don't appear anywhere else)
+python3 -m pip install -e git+https://github.com/silnrsi/pysilfont.git#egg=pysilfont --src "$HOME"/src
 # moving to the root of the user's home directory so that src/ folder for editable mode don't appear everywhere
-cd
 
-python3 -m pip install -e git+https://github.com/silnrsi/pysilfont.git@master#egg=pysilfont
+# components from main/master directly from upstream git repositories
+python3 -m pip install git+https://github.com/silnrsi/palaso-python.git git+https://github.com/googlefonts/GlyphsLib.git git+https://github.com/fonttools/ufoLib2.git git+https://github.com/fonttools/fonttools.git git+https://github.com/typemytype/glyphConstruction.git git+https://github.com/robotools/fontParts.git --use-pep517
 
-python3 -m pip install git+https://github.com/silnrsi/palaso-python.git@master#egg=palaso git+https://github.com/googlefonts/GlyphsLib.git@main#egg=glyphsLib git+https://github.com/fonttools/ufoLib2.git@main#egg=ufoLib2 git+https://github.com/fonttools/fonttools.git@main#egg=fontTools git+https://github.com/typemytype/glyphConstruction.git@master#egg=glyphConstruction git+https://github.com/robotools/fontParts.git@master#egg=fontParts
-
+# components from stable releases on pypi
 python3 -m pip install fs mutatorMath defcon fontMath lxml
 
 echo ""
