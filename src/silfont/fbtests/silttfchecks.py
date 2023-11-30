fdc6ab7549fcb3013c72b78cb36bc6771c371687
Nicolas Spalinger <nicolas.spalinger@sil.org>
2023-11-30 11:45:16 -0600

    Added simple WARN check to keep OFL-FAQ version current

--- src/silfont/fbtests/silttfchecks.py
+++ src/silfont/fbtests/silttfchecks.py
@@ -248,3 +248,31 @@ def org_sil_number_widths(ttFont, config):
         if figuremess: yield WARN, figuremess
     else:
         yield PASS, "All number widths are OK"
+
+
+
+@check(
+    id="org.sil/check/is_OFL_FAQ_current",
+    rationale="Although optional, it's recommended to have the current version of the OFL-FAQ in your font project",
+)
+def org_sil_check_is_ofl_faq_current(family_directory):
+    """Is the OFL FAQ current?"""
+
+    import os
+    parent_path = os.path.abspath(os.path.join(family_directory, os.pardir))
+    faq_file = os.path.join(parent_path, "OFL-FAQ.txt")
+    if not os.path.exists(faq_file):
+        yield WARN, Message(
+            "OFL-FAQ.txt file not found",
+            "The OFL-FAQ.txt file is missing. Although optional, it's recommended to include it in your project. Get the most current version from https://openfontlicense.org",
+        )
+    else:
+        with open(faq_file,'r',encoding='utf-8') as file:
+            data = file.read()
+            if not "Version 1.1-update7" in data:
+                yield WARN, Message(
+                    "FAQ is old",
+                    "The OFL-FAQ.txt file in this font project folder is not the most current version, get the latest (1.1-update7 from November 2023) from https://openfontlicense.org",
+                    )
+            else:
+                yield PASS, Message("ok", "OFL FAQ is current.")
