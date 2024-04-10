6766655662064a6f9d3e0f4f63593c6ae27db2aa
Nicolas Spalinger <nicolas.spalinger@sil.org>
2024-04-10 15:41:03 -0500

    Adjusted the shebang for helper scripts

--- src/silfont/fbtests/curate.py
+++ src/silfont/fbtests/curate.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+__url__ = 'https://github.com/silnrsi/pysilfont'
+__copyright__ = 'Copyright (c) 2024 SIL International (https://www.sil.org)'
+__license__ = 'Released under the MIT License (https://opensource.org/licenses/MIT)'
+__author__ = 'David Raymond'
 
 from sys import argv
 from csv import writer as csvwriter
--- src/silfont/fbtests/runfb.py
+++ src/silfont/fbtests/runfb.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 __url__ = 'https://github.com/silnrsi/pysilfont'
 __copyright__ = 'Copyright (c) 2024 SIL International (https://www.sil.org)'
 __license__ = 'Released under the MIT License (https://opensource.org/licenses/MIT)'
